babf8a605cbf3de86d82b9b4db3bd46f976327d3
bejbaryskusajyn-prog <bejbaryskusajyn@gmail.com>
2026-06-09 17:57:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let Year = 2026
+
+const Gender = "Мужчина"
+Gender = "!!!"
+
+console.log(Year)
+
+Year = Year + 1
+console.log(Year)
+Year += 1
+console.log(Year)
+Year++
+
+console.log(`я ${Gender}   который  живет в ${ Year }   году`)
+alert("привет!")
+prompt("вопрос")
